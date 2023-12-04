f02be5853f610500424efd69950fce1a5b75cc21
James Vickers <jamesvickers@jamess-imac.home>
2023-12-04 16:33:59 +0000

    CHORE: add interface and type definitions associated with task item

---
+++ src/interfaces/task.ts
@@ -0,0 +1,10 @@
+import { Document } from 'mongoose';
+import { Priority } from '../types/types';
+
+export default interface ITask extends Document {
+    assignee: string;
+    description: string;
+    priority: Priority;
+    completeByDate: Date; 
+}
+
