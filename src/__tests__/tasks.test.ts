24339921341ebb4e1d25380b44fe573c57957937
James Vickers <jamesvickers@jamess-imac.home>
2023-12-14 10:31:49 +0000

    FEAT: configure jest and add initial dummy test

---
+++ src/__tests__/tasks.test.ts
@@ -0,0 +1,9 @@
+describe('Tasks: ', () => {
+    describe('getAllTasks | GET | /tasks/get/all: ', () => {
+        describe('given a valid request: ', () => {
+            it('should return response code 200', () => {
+                expect(true).toBe(true);
+            })
+        })
+    })
+})
