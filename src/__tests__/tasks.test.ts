9c5a12c1531bf4d5aabb2a40e48caac893a1b4c5
James Vickers <jamesvickers@jamess-imac.home>
2023-12-14 14:51:53 +0000

    FEAT: establish test mongoDB connection in jest test and set up and tear down new task

--- src/__tests__/tasks.test.ts
+++ src/__tests__/tasks.test.ts
@@ -1,19 +1,67 @@
 import request from 'supertest'
+import mongoose from 'mongoose';
 import config from '../config/config';
 import app from '../app';
+import { ITask, TaskModel } from '../models/task';
 
 describe('Tasks: ', () => {
-    describe('getAllTasks | GET | /tasks/get/all: ', () => {
+    let testDatabaseConnection;
+    let createdTasks: ITask[];
+
+    beforeAll(async () => {
+        // Connect to a separate test database
+        testDatabaseConnection = await mongoose.connect(config.mongo.url, config.mongo.options)
+    });
+
+    afterAll(async () => {
+        // Disconnect from the test database after all tests
+        await mongoose.connection.close();
+    });
+
+    beforeEach(async () => {
+        // Create test data before each test
+        createdTasks = await TaskModel.create([
+            { assignee: 'Assignee 1', description: 'Description 1', priority: 1 },
+            { assignee: 'Assignee 2', description: 'Description 2', priority: 2 },
+        ]);
+        console.log('createdTasks: ', createdTasks)
+    });
+
+    afterEach(async () => {
+        // Remove test data after each test
+        await TaskModel.deleteMany({});
+    });
+
+    describe('/tasks/get/all: ', () => {
         describe('given a valid request: ', () => {
-            it('should return response code 200', async () => {
-                expect(true).toBe(true);
-                const getAllTasksEndpoint = '/tasks/get/all';
+            const getAllTasksEndpoint = '/tasks/get/all';
+            it('should return response code 200', async () => { // expect(true).toBe(true);
 
                 const response = await request(app).get(getAllTasksEndpoint).send();
 
                 // console.log('response: ', response);
                 expect(response.statusCode).toBe(200);
+
             })
+            // it('should return an array of tasks', async () => {
+            //     // expect(true).toBe(true);
+
+            //     const { body:  { message, count, tasks } } = await request(app).get(getAllTasksEndpoint).send();
+
+            //     console.log('tasks: ', tasks);
+
+            //     expect(tasks).toEqual(
+            //         expect.arrayContaining(
+            //             createdTasks.map((task) => expect.objectContaining({
+            //                 _id: task._id,
+            //                 assignee: task.assignee,
+            //                 description: task.description,
+            //                 priority: task.priority,
+            //                 // Add other fields to check as needed
+            //             }))
+            //         )
+            //     );
+            // })
         })
     })
 })
