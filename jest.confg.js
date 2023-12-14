ae1f142c27ec91a7d9abf08ee65fcc5368278266
James Vickers <jamesvickers@jamess-imac.home>
2023-12-14 10:59:26 +0000

    CHORE: install babel to make jest tests work with ESmodules import export syntax

--- jest.confg.js
+++ jest.confg.js
@@ -1,5 +1,5 @@
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node'
-    
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }
--- src/__tests__/tasks.test.ts
+++ src/__tests__/tasks.test.ts
@@ -1,8 +1,21 @@
+import request from 'supertest'
+import config from '../config/config';
+import app from '../app';
+
+// const request = require('supertest');
+
 describe('Tasks: ', () => {
     describe('getAllTasks | GET | /tasks/get/all: ', () => {
         describe('given a valid request: ', () => {
-            it('should return response code 200', () => {
-                expect(true).toBe(true);
+            it('should return response code 200', async () => {
+                 expect(true).toBe(true);
+                // const baseUrl = `http://localhost:${config.server.port}`;
+                // const getAllTasksEndpoint = '/get/all/tasks';
+                // const endpoint = `${baseUrl}${getAllTasksEndpoint}`;
+
+                // const response = await request(app).get(endpoint).send();
+
+                // expect(response.statusCode).toBe(200)
             })
         })
     })
