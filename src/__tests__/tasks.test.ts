621156238515a3a8d3bfad4b00f27bb7596bba08
James Vickers <jamesvickers@jamess-imac.home>
2023-12-14 11:37:49 +0000

    FIX: getAllTasks endpoint configured in jest test

--- src/__tests__/tasks.test.ts
+++ src/__tests__/tasks.test.ts
@@ -2,20 +2,17 @@ import request from 'supertest'
 import config from '../config/config';
 import app from '../app';
 
-// const request = require('supertest');
-
 describe('Tasks: ', () => {
     describe('getAllTasks | GET | /tasks/get/all: ', () => {
         describe('given a valid request: ', () => {
             it('should return response code 200', async () => {
-                 expect(true).toBe(true);
-                // const baseUrl = `http://localhost:${config.server.port}`;
-                // const getAllTasksEndpoint = '/get/all/tasks';
-                // const endpoint = `${baseUrl}${getAllTasksEndpoint}`;
+                expect(true).toBe(true);
+                const getAllTasksEndpoint = '/tasks/get/all';
 
-                // const response = await request(app).get(endpoint).send();
+                const response = await request(app).get(getAllTasksEndpoint).send();
 
-                // expect(response.statusCode).toBe(200)
+                // console.log('response: ', response);
+                expect(response.statusCode).toBe(200);
             })
         })
     })
