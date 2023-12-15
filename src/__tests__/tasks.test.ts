75c45a419f4ae1069b16efe09989154b6004a8cc
James Vickers <jamesvickers@jamess-imac.home>
2023-12-15 16:20:57 +0000

    FEAT: add more test scenarios and comments for task endpoints

--- src/__tests__/tasks.test.ts
+++ src/__tests__/tasks.test.ts
@@ -32,9 +32,23 @@ describe('Tasks: ', () => {
     });
 
     describe('/tasks/get/all: ', () => {
+        const getAllTasksEndpoint = '/tasks/get/all';
+
+        describe('given an invalid request: ', () => {
+            const unexpectedParam = { unexpectedParam: 'unexpectedParam' }
+            it('should return code 500', async () => {
+                const response = await request(app).get(getAllTasksEndpoint).send(unexpectedParam);
+                
+                expect(response.statusCode).toBe(500);
+            })
+
+            // TODO: scenarios
+
+            // should return the error message as a string
+            // should return the error
+        })
+
         describe('given a valid request: ', () => {
-            const getAllTasksEndpoint = '/tasks/get/all';
-            
             it('should return code 200', async () => {
                 const response = await request(app).get(getAllTasksEndpoint).send();
                 
@@ -63,6 +77,33 @@ describe('Tasks: ', () => {
                         }))
                     )
                 );
+            })
+        })
+    });
+
+    /* WIP: /tasks/create/task */
+
+    // Scenarios
+
+    //      given an invalid request
+    //          should return code 500
+    //          should return the error message as a string
+    //          should return the error
+    //      given a valid request
+    //          should return code 201
+    //          should return the correct success message
+    //          should return the newly created task
+    //          shoult return task containing correct properties and values
+
+    describe('/tasks/create/task: ', () => {
+        const testTask = {};
+        describe('given an invalid request: ', () => {
+            const createTaskEndpoint = '/tasks/create/task';
+
+            it('should return code 201', async () => {
+                // const response = await request(app).get(createTaskEndpoint).send();
+                
+                // expect(response.statusCode).toBe(201);
             })
         })
     })
